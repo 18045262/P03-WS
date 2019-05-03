243b316d14f25feb873ff08a4acdc3d2967ff6d2
TAN JIE YIN <18045262@rp.edu.sg>
2019-05-03 22:15:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/simpleapps/MainActivity.java
@@ -0,0 +1,53 @@
+package sg.edu.rp.c346.simpleapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+    ToggleButton tbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editText);
+        tbtn = findViewById(R.id.toggleButton2);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etInput.getText().toString();
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+            }
+        }
+        );
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tbtn.isChecked()){
+                    etInput.setEnabled(true);
+                }
+                else{
+                    etInput.setEnabled(false);
+                }
+            }
+        }
+
+
+        );
+    }
+
+}
